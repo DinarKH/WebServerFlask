c604b5fc7608faa4ff4bcfdf48ee3d567f151e76
Dinar5 <>
2019-07-24 11:21:15 +0400

    fixed post delete and added ttl

--- WebServerFlask/routes.py
+++ WebServerFlask/routes.py
@@ -5,6 +5,7 @@ from WebServerFlask import app, bcrypt, db, r_client
 from flask_login import login_user, current_user, logout_user, login_required
 import datetime, time
 
+
 @app.route('/')
 def home():
     return render_template('home.html')
@@ -13,9 +14,14 @@ def home():
 @app.route('/posts/', methods=['GET', 'POST'])
 @login_required
 def posts_page():
+    dt = datetime.datetime.now()
+    s = time.mktime(dt.timetuple())
+    print('Current time is {}'.format(s))
+    r_client.zremrangebyscore('post_set', min='-inf', max=s)
     if request.method == 'POST':
-        print(request.values.get('post_name'))
-    redis_posts= r_client.zrange('post_set', 0, -1)
+        r_client.zrem('post_set', request.values.get('post_name'))
+        return redirect(url_for('posts_page'))
+    redis_posts = r_client.zrange('post_set', 0, -1)
     posts = Posts.query.all()
     return render_template('post.html', posts=posts, redis_posts=redis_posts)
 
@@ -28,6 +34,7 @@ def post_delete(post_id):
     db.session.commit()
     flash('Post was deleted')
     return redirect(url_for('posts_page'))
+
 
 @app.route('/register/', methods=['GET'])
 def register():
@@ -76,7 +83,7 @@ def post_new():
         dt = datetime.datetime.now()
         s = time.mktime(dt.timetuple())
         print('Current time is {}'.format(s))
-        r_client.zadd('post_set', {form.content.data: s + 110})
+        r_client.zadd('post_set', {form.content.data: s + 50})
         # print(r_server.zremrangebyscore('post_set', min='-inf', max=s))
         # print(r_client.zrange('post_set', 0, -1, withscores=True))
         # post = Posts(title=form.title.data, content=form.content.data)
